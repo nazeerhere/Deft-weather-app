9ffccd22d2c7d4e33b9faac21608038ab38470d3
nazeer171 <nazeer3886@gmail.com>
2021-08-11 18:27:41 -0500

    over called api finish tomorrow, have urls and db setup

--- backend/urls.js
+++ backend/urls.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const axios = require("axios")
 
 const locations = require("./models/location-model")
 
@@ -7,8 +8,34 @@ router.get("/history", async (req, res, next) => {
     try {
         const location = await locations.find({})
         res.json(location)
+    } catch(err) {
+        next(err)
     }
-    catch(err) {
+})
+
+router.post("/history", (req, res, next) => {
+
+    try {
+        const newLoc = locations.create(req.body)
+        let data = ""
+
+        axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=t9EfppOfT3Hm81oPjoj1OoAieF6JGGAV&q=chicago`)
+        .then(res2 => {
+            data = res2.data
+            console.log(res2)
+        })
+        .then(_ => {
+            res.json({
+                Location: req.body,
+                message: `good work!!`,
+                data: data
+            })
+        })
+        
+        console.log("something happening")
+        console.log(req.body)
+
+    } catch(err) {
         next(err)
     }
 })
