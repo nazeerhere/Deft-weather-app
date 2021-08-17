a4797805075e615d935ffd37e460e4067199c386
nazeer171 <nazeer3886@gmail.com>
2021-08-17 11:01:53 -0500

    functional backend setup

--- backend/urls.js
+++ backend/urls.js
@@ -15,23 +15,76 @@ router.get("/history", async (req, res, next) => {
 
 router.post("/history", (req, res, next) => {
 
+    let prototype = {
+        date: "",
+        Temp: {
+            min: {},
+            max: {}
+        } ,
+        Day: {
+            Icon: 0,
+            IconPhrase: "",
+            HasProcipitation: Boolean
+        },
+        Night: {
+            Icon: 0,
+            IconPhrase: "",
+            HasProcipitation: Boolean
+        }
+    }
+
     try {
         const newLoc = locations.create(req.body)
-        let data = ""
+        let data = []
+        let arr = []
 
-        axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=t9EfppOfT3Hm81oPjoj1OoAieF6JGGAV&q=chicago`)
+        axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=t9EfppOfT3Hm81oPjoj1OoAieF6JGGAV&q=${req.body.Location}`)
         .then(res2 => {
-            data = res2.data
-            console.log(res2)
-        })
-        .then(_ => {
-            res.json({
-                Location: req.body,
-                message: `good work!!`,
-                data: data
+            console.log(res2.data[0].Key)
+            
+            axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${res2.data[0].Key}?apikey=t9EfppOfT3Hm81oPjoj1OoAieF6JGGAV`)
+            .then(res3 => {
+                data = res3.data.DailyForecasts
+                console.log(data.length)
+                console.log(data)
+
+                for(let i = 0; i<data.length; i++) {
+                    prototype = {
+                        date: data[i].Date,
+                        Temp: {
+                            min: data[i].Temperature.Minimum,
+                            max: data[i].Temperature.Maximum
+                        } ,
+                        Day: {
+                            Icon: data[i].Day.Icon,
+                            IconPhrase: data[i].Day.IconPhrase,
+                            HasProcipitation: data[i].Day.HasPrecipitation
+                        },
+                        Night: {
+                            Icon: data[i].Night.IconPhrase,
+                            IconPhrase: data[i].Night.IconPhrase,
+                            HasProcipitation: data[i].Night.HasPrecipitation
+                        }
+                    }
+
+                    console.log(prototype)
+                    arr.push(prototype)
+                }
             })
+
+            setTimeout(() => {
+                
+                res.json({
+                    Location: req.body,
+                    message: `good work!!`,
+                    data: arr
+                })
+            }, 500);
         })
         
+        setTimeout(() => {
+            console.log(arr)
+        }, 500);
         console.log("something happening")
         console.log(req.body)
 
